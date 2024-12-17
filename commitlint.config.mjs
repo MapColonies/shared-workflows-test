3431de77c219eb7af178d95820fe9757372e2ba3
netanelC <netanel153@gmail.com>
2024-12-17 13:18:17 +0200

    style(repo): format commitlint file

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,16 +1,20 @@
 export default {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-empty': [2, 'never'],
-        'scope-enum': [2, 'always', [
-            'repo',
-            'commitlint',
-            'pull-request', 
-            'npm-publish', 
-            'push-docker', 
-            'push-helm', 
-            'update-artifacts'
-        ]],
-        'header-min-length': [2, 'always', 10]
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [2, "never"],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "repo",
+        "commitlint",
+        "pull-request",
+        "npm-publish",
+        "push-docker",
+        "push-helm",
+        "update-artifacts",
+      ],
+    ],
+    "header-min-length": [2, "always", 10],
+  },
 };
