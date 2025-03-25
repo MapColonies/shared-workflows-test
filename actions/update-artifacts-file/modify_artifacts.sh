75941970e4f8da4dc26671906ba12a5ee37b6cc3
michalby24 <michalby24@gmail.com>
2025-03-25 17:48:19 +0200

    test(update-artifacts-file): test other logic

--- actions/update-artifacts-file/modify_artifacts.sh
+++ actions/update-artifacts-file/modify_artifacts.sh
@@ -3,26 +3,15 @@
 echo "$SCOPE" | tr ',' '\n' | while read -r element; do
   # Check if the directory exists
   if [ -d "$element" ]; then
-    echo "Changing directory to $REPOSITORY/$element"
-    pushd "$element"
-    node -e "
-    const fs = require('fs');
-    const path = './artifacts.json';
-    let data = {};
-    // Check if 'artifacts.json' file already exists
-    if (fs.existsSync(path)) {
-      data = JSON.parse(fs.readFileSync(path, 'utf8'));
-    }
-    // Ensure nested structure: docker -> registry -> scope -> artifact:tag
-    if (!data['$TYPE']['$REGISTRY']) data['$TYPE']['$REGISTRY'] = {};
-    const compositeKey = '${element}/$ARTIFACT_NAME';
-    // Assign the artifact tag to the composite key
-    data['$TYPE']['$REGISTRY'][compositeKey] = '$ARTIFACT_TAG';
-    // Writing new file contents
-    fs.writeFileSync(path, JSON.stringify(data, null, 2));
-    "
-    # go back to before pushd
-    popd
+    path="$element/artifacts.json"
+    # Create a default empty JSON if the file doesn't exist
+    [[ -f "$path" ]] || echo '{}' > "$path"
+    # Ensure nested structure and assign artifact tag
+    jq --arg type "$TYPE" \
+      --arg registry "$REGISTRY" \
+      --arg key "${element}/$ARTIFACT_NAME" \
+      --arg tag "$ARTIFACT_TAG" \
+      '.[$type][$registry][$key] = $tag' "$path" > tmp.json && mv tmp.json "$path"
   else
     echo "Directory $REPOSITORY/$element does not exist"
   fi
