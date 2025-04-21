ea3ce7aec8932421867e8d19a0819fab71c2399a
michalby24 <michalby24@gmail.com>
2025-04-21 11:28:24 +0300

    style(update-artifacts-file): change scope to domain

--- actions/update-artifacts-file/commit_changes.sh
+++ actions/update-artifacts-file/commit_changes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd $SCOPE
+cd $DOMAIN
 git config --global user.name "mapcolonies[bot]"
 git config --global user.email "devops[bot]@mapcolonies.com"
 
 git add .
 # Add "|| true" for not failing on this line 1
-git commit -m "chore: update artifacts.json for $SCOPE" -m "with $TYPE artifact: $ARTIFACT_NAME:$ARTIFACT_TAG" || true
+git commit -m "chore: update artifacts.json for $DOMAIN" -m "with $TYPE artifact: $ARTIFACT_NAME:$ARTIFACT_TAG" || true
 
 success=false
 for ((i=1; i<=5; i++)); do
--- actions/update-artifacts-file/modify_artifacts.sh
+++ actions/update-artifacts-file/modify_artifacts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Check if the directory exists
-if [ -d "$SCOPE" ]; then
-  path="$SCOPE/artifacts.json"
+if [ -d "$DOMAIN" ]; then
+  path="$DOMAIN/artifacts.json"
   # Create a default empty JSON if the file doesn't exist
   [[ -f "$path" ]] || echo '{}' > "$path"
   # Ensure nested structure and assign artifact tag
   jq --arg type "$TYPE" \
     --arg registry "$REGISTRY" \
-    --arg key "${element}/$ARTIFACT_NAME" \
+    --arg key "${DOMAIN}/$ARTIFACT_NAME" \
     --arg tag "$ARTIFACT_TAG" \
     '.[$type][$registry][$key] = $tag' "$path" > tmp.json && mv tmp.json "$path"
 else
-  echo "Directory $REPOSITORY/$SCOPE does not exist"
+  echo "Directory $REPOSITORY/$DOMAIN does not exist"
 fi
